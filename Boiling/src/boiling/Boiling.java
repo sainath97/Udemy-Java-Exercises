f79e66bc979cf392ff44df3814fb47a239cdc240
Tulluri Krishna Sainath <krishnasainath1997@gmail.com>
2018-04-27 10:57:54 +0530

    Add files via upload

---
+++ Boiling/src/boiling/Boiling.java
@@ -0,0 +1,29 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package boiling;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author lenovo
+ */
+public class Boiling {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        System.out.println("Enter the temperature of water: ");
+        int temp=sc.nextInt();
+        if(temp>=100)
+            System.out.println("The water is boiling.");
+        else
+            System.out.println("The water is not boiling.");
+    }
+    
+}
